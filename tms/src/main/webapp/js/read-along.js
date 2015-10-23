39d71d0e8571a2bf5d6955e2d7fafa3c4a0f6169
mdevulap <mdevuap@uno.edu>
2015-10-23 05:30:15 -0700

    added a basic template of read-long script file

---
+++ tms/src/main/webapp/js/read-along.js
@@ -0,0 +1,27 @@
+/**
+ Created by muralidhar and surya on 9/24/2015.
+ 
+ ****/
+
+
+var ReadAlong = {
+    text_element: null,
+    audio_element: null,
+    autofocus_current_word: false,
+    flag:null,
+
+  
+   
+  
+    
+
+    addEventListeners: function () {
+       
+
+        }, false);
+
+ 
+
+        
+    }
+};
