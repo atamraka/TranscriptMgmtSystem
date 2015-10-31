54c8eafd975f6c3decba21a73acb518948c65920
nmaipala <nmaipala@uno.edu>
2015-10-31 14:48:27 -0500

    added bookmark and endbookmark functions

--- tms/src/main/webapp/js/read-along.js
+++ tms/src/main/webapp/js/read-along.js
@@ -11,9 +11,9 @@ var ReadAlong = {
     text_element: null,
     audio_element: null,
     autofocus_current_word: false,
-    flag:null,
+    flag: null,
 
-   words: [],
+    words: [],
     bookMarkWords: [],
     init: function (args) {
         var name;
@@ -27,9 +27,9 @@ var ReadAlong = {
         this.selectCurrentWord();
 
     },
-    /** 
+    /**
      * @Nitesh agarawal extended code on 10/24/2015
-     * 
+     *
      * Build an index of all of the words that can be read along with their begin,
      * and end times, and the DOM element representing the word.
      */
@@ -49,10 +49,10 @@ var ReadAlong = {
         });
     },
 
-    
+
     /**
      * @Mahaveer on 10/26/2015
-     * 
+     *
      * From the audio's currentTime, find the word that is currently being played
      * @todo this would better be implemented as a binary search
      */
@@ -88,45 +88,45 @@ var ReadAlong = {
     _current_end_select_timeout_id: null,
     _current_next_select_timeout_id: null,
 
-      /**
-     * 
+    /**
+     *
      * @Vivek Bhatnagar on 10/25/2015
      * Select the current word and set timeout to select the next one if playing
      */
-    selectCurrentWord: function() {
-     var that = this;
-     var current_word = this.getCurrentWord();
-     var is_playing = !this.audio_element.paused;
-      if (that.flag) {
-         console.log(that.flag);
-         for (var i in that.bookMarkWords) {
-             if (that.bookMarkWords[i] == current_word.index) {
-                 current_word.element.classList.add('bookmark');
-                 if (current_word.element.classList.contains('bookmark')) {
-                     if (that.autofocus_current_word) {
-                         current_word.element.focus();
-                     }
-                 }
-             }
+    selectCurrentWord: function () {
+        var that = this;
+        var current_word = this.getCurrentWord();
+        var is_playing = !this.audio_element.paused;
+        if (that.flag) {
+            console.log(that.flag);
+            for (var i in that.bookMarkWords) {
+                if (that.bookMarkWords[i] == current_word.index) {
+                    current_word.element.classList.add('bookmark');
+                    if (current_word.element.classList.contains('bookmark')) {
+                        if (that.autofocus_current_word) {
+                            current_word.element.focus();
+                        }
+                    }
+                }
 
-         }
-      }
-          if (!current_word.element.classList.contains('speaking')) {
-         this.removeWordSelection();
-         current_word.element.classList.add('speaking');
-         if (this.autofocus_current_word) {
-             current_word.element.focus();
-         }
-         }
-      },
+            }
+        }
+        if (!current_word.element.classList.contains('speaking')) {
+            this.removeWordSelection();
+            current_word.element.classList.add('speaking');
+            if (this.autofocus_current_word) {
+                current_word.element.focus();
+            }
+        }
+    },
 
-/***
+    /***
      * @vishal thakur 10/26/2015
      * added a function once the selected word highlighed ,removing the word from classlist
-     * 
+     *
      */
 
-    removeWordSelection: function() {
+    removeWordSelection: function () {
         // There should only be one element with .speaking, but selecting all for good measure
         var spoken_word_els = this.text_element.querySelectorAll('span[data-begin].speaking');
         Array.prototype.forEach.call(spoken_word_els, function (spoken_word_el) {
@@ -136,41 +136,38 @@ var ReadAlong = {
 
 
     /***
-    @ vinay reddy on 10/24/2015
-    
-    Added all the listeners when user performs any actions
-   
-   */
-  
-       addEventListeners: function () {
-      var that = this;
-      var word_book=[];
-      var temp;
+     @ vinay reddy on 10/24/2015
 
-          /**
-       * Select next word (at that.audio_element.currentTime) when playing begins
-       */
-      that.audio_element.addEventListener('play', function () {
-          that.selectCurrentWord();
-          that.text_element.classList.add('speaking');
-          console.log(that.flag);
+     Added all the listeners when user performs any actions
 
-      }, false);
-    },
-  };
+     */
+    addEventListeners: function () {
+        var that = this;
+        var word_book = [];
+        var temp;
+
+        /**
+         * Select next word (at that.audio_element.currentTime) when playing begins
+         */
+        that.audio_element.addEventListener('play', function () {
+            that.selectCurrentWord();
+            that.text_element.classList.add('speaking');
+            console.log(that.flag);
+
+        }, false);
 
 
-/**
- * @vinay reddy on 10/26/2015
- * Abandon seeking the next word because we're paused
- */
-that.audio_element.addEventListener('pause', function (e) {
-    that.selectCurrentWord(); // We always want a word to be selected
-    that.text_element.classList.remove('speaking');
-}, false);
+        /**
+         * @vinay reddy on 10/26/2015
+         * Abandon seeking the next word because we're paused
+         */
+        that.audio_element.addEventListener('pause', function (e) {
+            that.selectCurrentWord(); // We always want a word to be selected
+            that.text_element.classList.remove('speaking');
+        }, false);
 
 
-/**
+        /**
          * Nitesh Agarwal on 10/27/2015
          * Seek by selecting a word (event delegation)
          */
@@ -184,19 +181,20 @@ that.audio_element.addEventListener('pause', function (e) {
             that.audio_element.currentTime = that.words[i].begin + 0.01; //Note: times apparently cannot be exactly set and sometimes select too early
             that.selectCurrentWord();
         }
+
         that.text_element.addEventListener('click', on_select_word_el, false);
         that.text_element.addEventListener('keypress', function (e) {
-            if ( (e.charCode || e.keyCode) === 13 /*Enter*/) {
+            if ((e.charCode || e.keyCode) === 13 /*Enter*/) {
                 on_select_word_el.call(this, e);
             }
         }, false);
-/**
-         * @ Ravi charan 
-         * 
+        /**
+         * @ Ravi charan
+         *
          *added  Spacebar toggles playback
          */
         document.addEventListener('keypress', function (e) {
-            if ( (e.charCode || e.keyCode) === 32 /*Space*/) {
+            if ((e.charCode || e.keyCode) === 32 /*Space*/) {
                 e.preventDefault();
                 if (that.audio_element.paused) {
                     that.audio_element.play();
@@ -207,9 +205,9 @@ that.audio_element.addEventListener('pause', function (e) {
             }
         }, false);
 
-        
+
         /**
-         * 
+         *
          *  Vinay Reddy on 10/29/2015
          * Select a word when seeking
          */
@@ -225,7 +223,7 @@ that.audio_element.addEventListener('pause', function (e) {
                 }, 500);
             }
         }, false);
-        
+
         /**
          * @Vinay Reddy on 10/30/2015
          * Select a word when seeking
@@ -233,3 +231,29 @@ that.audio_element.addEventListener('pause', function (e) {
         that.audio_element.addEventListener('ratechange', function (e) {
             that.selectCurrentWord();
         }, false);
+
+
+        /***
+         @suryanarayna  on 10/12/2015
+         added below two function when user clicks on bookmark and EndBookMark buttons
+         */
+
+        document.getElementById("Bookmark").addEventListener('click', function () {
+            var current_word = that.getCurrentWord();
+            if (temp != false) {
+                word_book.push(current_word.index);
+
+            }
+        });
+
+        /** suryanarayana */
+
+        document.getElementById("EndBookmark").addEventListener('click', function () {
+            temp = false;
+            that.CopyArraysBookMarks(word_book);
+            that.audio_element.load();
+
+
+        });
+    }
+};
