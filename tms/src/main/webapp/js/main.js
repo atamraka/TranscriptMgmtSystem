60d0441861a0a69ab44dd5cc8487684f497fcfeb
nmaipala <nmaipala@uno.edu>
2015-11-10 20:47:19 -0600

    changed load_main function  for loading functionality

--- tms/src/main/webapp/js/main.js
+++ tms/src/main/webapp/js/main.js
@@ -1,28 +1,35 @@
 /**
  * Created by Anjila on 09/19/2015.
- * 
+ *
  * this page will load and send a arguments with text,audio,word focus to other script file
  * and also initialize the words with hightlight color
- * 
- * 
+ *
+ *  changed eventlistner with function load_main
+ *  modified by suryanarayana on 10/10/15
  */
-window.addEventListener('load', function (e) {
-    try {
-        var args = {
-            text_element: document.getElementById('passage-text'),
-            audio_element: document.getElementById('passage-audio'),
-            autofocus_current_word: document.getElementById('autofocus-current-word').checked,
-        };
-        transcriptor.init(args);
-        document.getElementById('autofocus-current-word').addEventListener('change', function (e) {
-            transcriptor.autofocus_current_word = this.checked;
-        });
-        
-    }
-    catch (err) {
-        console.error(err);
-    }
-    document.body.classList.add('initialized');
-    
+function load_main(value) {
+    if(value) {
+        try {
+            var args = {
+                text_element: document.getElementById('passage-text'),
+                audio_element: document.getElementById('passage-audio'),
+                autofocus_current_word: document.getElementById('autofocus-current-word').checked,
 
-},false);
+
+            };
+
+            transcriptor.init(args);
+            document.getElementById('autofocus-current-word').addEventListener('change', function (e) {
+                transcriptor.autofocus_current_word=this.checked;
+            });
+
+
+        }
+        catch (err) {
+            /* Log.error(err);*/
+        }
+        document.body.classList.add('initialized');
+
+    }
+
+};
