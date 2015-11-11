b1be88d73f54a93a8aff1c06aa81cc53d28b560b
VinayReddy <a.vinaykumarreddy@gmail.com>
2015-11-11 05:49:04 +0000

    Added key spies for methods

--- tms/src/test/js/read-alongSpec.js
+++ tms/src/test/js/read-alongSpec.js
@@ -8,3 +8,29 @@ describe('toThrow', function() {
 	        expect(object.getCurrentWord).toThrow(new Error("Unexpected error!"));
 	    });
 	});
+	
+	describe("Spies", function() {
+	 
+	it("Should spy on word list", function() {
+	    var spy = spyOn(ReadAlong,'generateWordList')
+	    ReadAlong.generateWordList();
+	    expect(spy).toHaveBeenCalled();
+	    
+	  });
+	
+	it("Should spy on CurrentWord if Word is Null", function() {
+		
+	    var spy = spyOn(ReadAlong,'getCurrentWord').and.returnValue('null')
+	    
+	    expect(ReadAlong.getCurrentWord()).toEqual('null');
+	    
+	  });
+	
+	
+	
+	it("Should spy on CurrentWord", function() {
+	    var spy = spyOn(ReadAlong,'getCurrentWord')
+	    ReadAlong.getCurrentWord();
+	    expect(spy).toHaveBeenCalled();
+	    
+	  });
